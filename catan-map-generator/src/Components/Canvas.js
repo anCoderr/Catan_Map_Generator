107932c19b73d781c45760baf465bfbc2c35d3ff
anCoderr <deyol.anurag998@gmail.com>
2021-05-15 16:02:29 +0530

    fully working product

--- catan-map-generator/src/Components/Canvas.js
+++ catan-map-generator/src/Components/Canvas.js
@@ -7,6 +7,7 @@ import pastureImage from "../assets/images/Main/pasture.png";
 import stoneImage from "../assets/images/Main/stone.png";
 import forestImage from "../assets/images/Main/forest.png";
 import desertImage from "../assets/images/Main/desert.png";
+import oceanImage from "../assets/images/Main/water.png";
 
 import styles from './Canvas.module.css';
 
@@ -28,6 +29,7 @@ const Canvas = (props) => {
             <img className={styles.tileImage} id="stone" alt="stone" src={stoneImage}></img>
             <img className={styles.tileImage} id="forest" alt="forest" src={forestImage}></img>
             <img className={styles.tileImage} id="desert" alt="desert" src={desertImage}></img>
+            <img className={styles.tileImage} id="ocean" alt="ocean" src={oceanImage}></img>
             <canvas className={styles.canvas} ref={canvasRef}/>
         </div>
 
@@ -38,17 +40,34 @@ const canvasDrawing = (canvas, context, mapObject) => {
         canvas.height = window.innerHeight;
         context.imageSmoothingEnabled = true;
         context.strokeStyle = "black";
-        context.fillStyle = "white";
-        context.clearRect(0, 0, canvas.width, canvas.height); 
-    
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
         let loadCount = 0, errorCount = 0;
-        const targetCount = 5;
+        const targetCount = 6;
         
         const factor = 0.866;
         const padding = canvas.height/200;
-        const size = canvas.height/10.39;
+        const size = canvas.height/12;
         const distY = (2*factor*size+padding)/2;
         const distX = (3*size + factor*2*padding)/2;
+        
+        const dp = (4*factor*padding)/2;
+
+        const d0 = size/2 + dp;
+        const d1 = size + dp;
+        const d2 = distX + dp + size/2;
+        const d3 = distX + dp + size;
+        const d4 = 2*distX + dp + size/2;
+        const d5 = 2*distX + dp + size;
+        const d6 = 5*distY + padding/2;
+        const d7 = 4*distY + padding/2;
+        const d8 = 3*distY + padding/2;
+        const d9 = 2*distY + padding/2;
+        const d10 = distY + padding/2; 
+
+        const boardSize = canvas.height/2;
+        const d = (canvas.width - 2*factor*boardSize)/2;
+        const t = (canvas.height - boardSize)/2; 
 
         const absoluteCenterX = canvas.width/2;
         const absoluteCenterY = canvas.height/2;
@@ -62,8 +81,10 @@ const canvasDrawing = (canvas, context, mapObject) => {
         let imageStone = document.getElementById("stone");
         let imageTree = document.getElementById("forest");
         let imageDesert = document.getElementById("desert");
+        let imageOcean = document.getElementById("ocean");
 
-        let imagesArray = {imageClay, imageHay, imagePasture, imageStone, imageTree};
+
+        let imagesArray = {imageClay, imageHay, imagePasture, imageStone, imageTree, imageOcean};
 
         const  drawOnTile = (index, centerX, centerY) => {
             for(let i = 0; i<19; i++) {
@@ -89,13 +110,21 @@ const canvasDrawing = (canvas, context, mapObject) => {
             context.drawImage(imageName, 0, 0, imageName.width, imageName.height, centerX - size, centerY - factor*size, 2*size, 2*factor*size);
 
             if(probabilityNumber !== 7) {
-                drawTokerCircle(centerX, centerY, 30, 40);
+                drawTokerCircle(centerX, centerY, 30, 40, probabilityNumber);
                 context.font = "bold " + size/4  +"px Arial";
                 context.textAlign = "center";
-                context.fillStyle = "grey";
+                context.fillStyle = probabilityTextColour(probabilityNumber);
                 context.fillText(probabilityNumber, centerX, centerY+size/12);
             }
             
+        }
+
+        const probabilityTextColour = (probabilityNumber) => {
+            if(probabilityNumber === 6 || probabilityNumber === 8) {
+                return "red";
+            } else {
+                return "black";
+            }
         }
 
         const drawTokerCircle = (centerX, centerY, radiusSmall, radiusBig) => {
@@ -123,6 +152,60 @@ const canvasDrawing = (canvas, context, mapObject) => {
             context.closePath();
             context.stroke();
             drawOnTile(index, centerX, centerY);
+        }
+
+        const waterDraw = () => {
+            console.log('Boom', canvas.width, canvas.height, d, t);
+            context.beginPath();
+            context.moveTo(canvas.width/2, 0);
+            context.lineTo(d, t);
+            context.lineTo(d, boardSize+t);
+            context.lineTo(canvas.width/2, canvas.height);
+            context.lineTo(canvas.width-d, canvas.height-t);
+            context.lineTo(canvas.width-d, t);
+            context.lineTo(canvas.width/2, 0);
+            // context.drawImage(imageOcean, d, 0, canvas.width - d,canvas.height)
+            context.fillStyle = "Navy";
+            context.fill();
+            context.stroke();
+        }
+
+        const landDraw = () => {
+            context.beginPath();
+            context.moveTo(absoluteCenterX - d0, absoluteCenterY - d6);
+            context.lineTo(absoluteCenterX - d1, absoluteCenterY - d7);
+            context.lineTo(absoluteCenterX - d2, absoluteCenterY - d7);
+            context.lineTo(absoluteCenterX - d3, absoluteCenterY - d8);
+            context.lineTo(absoluteCenterX - d4, absoluteCenterY - d8);
+            context.lineTo(absoluteCenterX - d5, absoluteCenterY - d9);
+            context.lineTo(absoluteCenterX - d4, absoluteCenterY - d10);
+            context.lineTo(absoluteCenterX - d5, absoluteCenterY);
+            context.lineTo(absoluteCenterX - d4, absoluteCenterY + d10);
+            context.lineTo(absoluteCenterX - d5, absoluteCenterY + d9);
+            context.lineTo(absoluteCenterX - d4, absoluteCenterY + d8);
+            context.lineTo(absoluteCenterX - d3, absoluteCenterY + d8);
+            context.lineTo(absoluteCenterX - d2, absoluteCenterY + d7);
+            context.lineTo(absoluteCenterX - d1, absoluteCenterY + d7);
+            context.lineTo(absoluteCenterX - d0, absoluteCenterY + d6);
+            context.lineTo(absoluteCenterX + d0, absoluteCenterY + d6);
+            context.lineTo(absoluteCenterX + d1, absoluteCenterY + d7);
+            context.lineTo(absoluteCenterX + d2, absoluteCenterY + d7);
+            context.lineTo(absoluteCenterX + d3, absoluteCenterY + d8);
+            context.lineTo(absoluteCenterX + d4, absoluteCenterY + d8);
+            context.lineTo(absoluteCenterX + d5, absoluteCenterY + d9);
+            context.lineTo(absoluteCenterX + d4, absoluteCenterY + d10);
+            context.lineTo(absoluteCenterX + d5, absoluteCenterY);
+            context.lineTo(absoluteCenterX + d4, absoluteCenterY - d10);
+            context.lineTo(absoluteCenterX + d5, absoluteCenterY - d9);
+            context.lineTo(absoluteCenterX + d4, absoluteCenterY - d8);
+            context.lineTo(absoluteCenterX + d3, absoluteCenterY - d8);
+            context.lineTo(absoluteCenterX + d2, absoluteCenterY - d7);
+            context.lineTo(absoluteCenterX + d1, absoluteCenterY - d7);
+            context.lineTo(absoluteCenterX + d0, absoluteCenterY - d6);
+            context.lineTo(absoluteCenterX - d0, absoluteCenterY - d6);
+            context.fillStyle = "SaddleBrown";
+            context.fill();
+            context.stroke();
         }
 
         const col1Draw = () => {
@@ -164,6 +247,8 @@ const canvasDrawing = (canvas, context, mapObject) => {
         const imageLoader = () => {
             const checkAllLoaded = function() {
                 if (loadCount + errorCount === targetCount) {
+                    waterDraw();
+                    landDraw();
                     mapDraw();
                 }
             };
@@ -177,6 +262,7 @@ const canvasDrawing = (canvas, context, mapObject) => {
             }; 
             const loadImages = () => {
                 if(loadCount + errorCount >= targetCount) {
+                    landDraw();
                     mapDraw();
                 } else {
                     for (var i = 0; i < targetCount; i++) {
