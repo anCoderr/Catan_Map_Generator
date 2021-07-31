f6b67129f38268550283b5d7dfc4d67555b8c460
anCoderr <deyol.anurag998@gmail.com>
2021-08-01 01:41:04 +0530

    working

--- catan-map-generator/src/Containers/Home.js
+++ catan-map-generator/src/Containers/Home.js
@@ -3,6 +3,7 @@ import mapGenerator from '../Logic/MapGenerator';
 import Board from './Board';
 import { connect } from 'react-redux';
 import * as actions from '../store/actions/actions'
+import Styles from './Home.module.css';
 
 const Home = props => {
     const onGenerateClickHandler = () => {
@@ -11,8 +12,14 @@ const Home = props => {
     }
     
     return (
-        <div>
-            <button onClick={onGenerateClickHandler}>Generate Map</button>
+        <div className={Styles.totalBlock}>
+            <div className={Styles.textBlock}>
+                <h1>CATAN BOARD GENERATOR</h1>
+                <h3>By anCoderr</h3>
+                <a href="https://github.com/anCoderr" target="_blank">https://github.com/anCoderr</a>
+                <br className={Styles.gap}/>
+                <button onClick={onGenerateClickHandler}>Generate Map</button>
+            </div>
             <Board/>
         </div>
     );
