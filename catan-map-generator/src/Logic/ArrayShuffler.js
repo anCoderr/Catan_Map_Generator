453b33118caad490c13eca1faf9d37823e69230f
anCoderr <deyol.anurag998@gmail.com>
2021-08-01 00:38:10 +0530

    rand func removed

--- catan-map-generator/src/Logic/ArrayShuffler.js
+++ catan-map-generator/src/Logic/ArrayShuffler.js
@@ -1,15 +1,9 @@
 const arrayShuffler = (array) => {
     for(let i = array.length-1; i>0; i--) {
         var date = new Date();
-        let j = randomNumberGenerator(date.getTime()*date.getTime(), 2, 0, 3);
-        console.log("Rand Working" + j);
-        // let j = Math.floor(Math.random()*(i+1));
+        let j = Math.floor(Math.random()*(i+1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-}
-
-const randomNumberGenerator = (seed, a, b, m) => {
-    return (a * seed + b) % m;
 }
 
 export default arrayShuffler;
