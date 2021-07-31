56d6214e67f38a9a11eaaed8009291194bedf4dc
anCoderr <deyol.anurag998@gmail.com>
2021-08-01 00:35:54 +0530

    random num generaotr added

--- catan-map-generator/src/Logic/ArrayShuffler.js
+++ catan-map-generator/src/Logic/ArrayShuffler.js
@@ -1,8 +1,15 @@
 const arrayShuffler = (array) => {
     for(let i = array.length-1; i>0; i--) {
-        let j = Math.floor(Math.random()*(i+1));
+        var date = new Date();
+        let j = randomNumberGenerator(date.getTime()*date.getTime(), 2, 0, 3);
+        console.log("Rand Working" + j);
+        // let j = Math.floor(Math.random()*(i+1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+}
+
+const randomNumberGenerator = (seed, a, b, m) => {
+    return (a * seed + b) % m;
 }
 
 export default arrayShuffler;
